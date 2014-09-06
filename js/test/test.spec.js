4bdb36199c122bb2c28d57c859bef4cbf35cbb76
James <james@jjgportfolio.com>
2014-09-06 06:53:39 +0100

    Added karma and bowerjs

---
+++ js/test/test.spec.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jgpro_000 on 06/09/14.
+ */
+define([], function() {
+    describe("A suite", function() {
+        it("contains spec with an expectation", function() {
+            expect(true).toBe(true);
+        });
+    });
+});
